f2e159c31452624dced5410d87159a346e42479e
enddl3788 <enddl3788@naver.com>
2023-08-10 16:06:02 +0900

    MVVM 패턴_2 # 폴더 변경시 적용되는 코드 변경

---
+++ app/src/main/java/military/gunbam/view/activity/BasicActivity.java
@@ -0,0 +1,17 @@
+package military.gunbam.view.activity;
+
+import android.os.Bundle;
+import android.view.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+// 모든 엑티비티에 중복되는 값
+public class BasicActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE); // Remove title
+    }
+}
